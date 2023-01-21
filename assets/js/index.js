05bed276a0f8b086a6443fef511917fc72c1d2d5
Mcc <macharitocruzuk@gmail.com>
2023-01-21 20:14:11 +0000

    add function for building time block and function for rowColor.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// Display the current day at the top of the calender when a user opens the planner.
 $("#currentDay").text(moment().format("dddd, MMMM Do "));
 //create an element( hours)
 let hours = [
@@ -11,33 +12,54 @@ let hours = [
   "16:00",
   "17:00",
 ];
-let $currentEvent = {};
-let $hoursOfDay = $("<.container>");
+let $timeBlock = $("<.container>");
 
-// Display the current day at the top of the calender when a user opens the planner.
-let $currentHour = moment().format("hA");
+let $currentTime = moment().format("hA");
+let $row = $(".row");
 
-renderEvent();
-function renderEvent() {
+buildPlanner();
+function buildPlanner() {
   // Present timeblocks for standard business hours when the user scrolls down.
   for (let i = 0; i < hours.length; i++) {
-    let hour = hours[i];
+    // let hour = hours[i];
+    // let $currentTime = $("<div>");
+
     let $currentTime = $(".row");
-    $currentTime.attr("date-time", hour);
+    $currentTime.text(hours[i]);
+    // $currentTime.attr("class", "row");
+    // $currentTime.addClass("col-2 ");
+    $(".container").append($("#hour").text(), $("textarea").val());
+
+    // $currentTime.attr("data-time", hour[i]);
   }
 }
-
+// console.log($(".hour"));
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-
+function rowColor() {
+  let $eventTime = $("#hour");
+  if ($eventTime < $currentTime) {
+    $row = $(".row").attr("class", ".past");
+  } else {
+    if ($eventTime > $currentTime) {
+      $row = $(".row").attr("class", ".future");
+    } else {
+      $row = $(".row").attr("class", "present");
+    }
+  }
+}
+rowColor();
 // Allow a user to enter an event when they click a timeblock
-
 // Save the event in local storage when the save button is clicked in that timeblock.
 
 $(".saveBtn").on("click", function (event) {
+  // event.preventDefault();
   $(".container").append($(".row").val());
+  $(".row").append($("#hour").val());
 
-  let eventInput = $("textarea").val();
-  localStorage.setItem("Event Input", eventInput);
+  let $eventTextInput = $("textarea").val();
+  let $eventTime = $("#hour").val();
+  localStorage.setItem("Event Input", $eventTextInput);
+  localStorage.setItem("Event Time", $eventTime);
 });
 
 // Persist events between refreshes of a page
