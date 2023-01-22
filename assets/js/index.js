6e6e9ee3e1b5a4f414f224b75ef053951d3c4dc1
Mcc <macharitocruzuk@gmail.com>
2023-01-22 18:48:56 +0000

    update loop for time block.

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,8 @@ let hours = [
 let $timeBlock = $("<.container>");
 
 let $currentTime = moment().format("hA");
+let isbefore = moment().isBefore($currentTime);
+let isafter = moment().isAfter($currentTime);
 let $row = $(".row");
 
 buildPlanner();
@@ -23,31 +25,35 @@ function buildPlanner() {
   for (let i = 0; i < hours.length; i++) {
     // let hour = hours[i];
     // let $currentTime = $("<div>");
-
     let $currentTime = $(".row");
+    let $eventInput = $("textarea");
     $currentTime.text(hours[i]);
-    // $currentTime.attr("class", "row");
-    // $currentTime.addClass("col-2 ");
-    $(".container").append($("#hour").text(), $("textarea").val());
+    $currentTime.attr("class", "row");
+    $eventInput.text("data-time", "$(#hour)");
+    $eventInput.attr("data-time", "$(#hour)");
+    $currentTime.addClass("col-2 ");
+    $(".container").append($(".row"));
+    $(".row").append($("#hour").text(), $("textarea").val());
 
+    // $("#hour").val().append($("textarea").text());
     // $currentTime.attr("data-time", hour[i]);
   }
 }
 // console.log($(".hour"));
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+rowColor();
 function rowColor() {
   let $eventTime = $("#hour");
-  if ($eventTime < $currentTime) {
+  if (($eventTime, (isbefore = true))) {
     $row = $(".row").attr("class", ".past");
   } else {
-    if ($eventTime > $currentTime) {
+    if (($eventTime, (isafter = true))) {
       $row = $(".row").attr("class", ".future");
     } else {
       $row = $(".row").attr("class", "present");
     }
   }
 }
-rowColor();
 // Allow a user to enter an event when they click a timeblock
 // Save the event in local storage when the save button is clicked in that timeblock.
 
@@ -56,7 +62,7 @@ $(".saveBtn").on("click", function (event) {
   $(".container").append($(".row").val());
   $(".row").append($("#hour").val());
 
-  let $eventTextInput = $("textarea").val();
+  let $eventTextInput = $("textarea").text();
   let $eventTime = $("#hour").val();
   localStorage.setItem("Event Input", $eventTextInput);
   localStorage.setItem("Event Time", $eventTime);
